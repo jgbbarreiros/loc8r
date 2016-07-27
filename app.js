7626eec878285c86ef11df1611053a2d72a5a36e
Joao Barreiros <jgbbarreiros@gmail.com>
2016-07-27 17:26:47 +0100

    Database connection. Belive!

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+require('./app_server/models/db');
 
 var routes = require('./app_server/routes/index');
 //var users = require('./app_server/routes/users');
@@ -15,7 +16,7 @@ app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
